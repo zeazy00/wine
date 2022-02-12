4082e26b4c144007573f0303c157c161d0eea08f
zeazy00 <ZeazyGame@gmail.com>
2022-02-12 22:53:45 +1000

    add Jinja2

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+FOUNDATION_YEAR = 1920
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ in "__main__":
+	env = Environment(
+		loader=FileSystemLoader('.'),
+		autoescape=select_autoescape(['html'])
+	)
+	template = env.get_template('template.html')
+
+	rendered_page = template.render(
+		working_years=datetime.datetime.now().year - FOUNDATION_YEAR
+	)
+	template = env.get_template('template.html')
+
+	with open('index.html', 'w', encoding="utf8") as file:
+		file.write(rendered_page)
+	
+	server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+	server.serve_forever()
